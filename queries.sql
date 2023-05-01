5c5b4311a9f84165c77ffbd677a4437ebae921a1
c00p75_ <georgecoopmsapenda@gmail.com>
2023-05-01 21:11:36 +0200

    Add queries.

--- queries.sql
+++ queries.sql
@@ -1,5 +1,8 @@
 /*Queries that provide answers to the questions from all projects.*/
 
+-- Find all animals whose name ends in "mon".
+SELECT * FROM animals WHERE name LIKE '%mon';
+
 -- List the name of all animals born between 2016 and 2019.
 SELECT name FROM animals WHERE date_of_birth BETWEEN '2016/01/01' AND '2019/12/31'; 
 
