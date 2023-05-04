e698a4ae492d0264fc25e94d85452e9caa75bee4
c00p75_ <georgecoopmsapenda@gmail.com>
2023-05-04 18:13:04 +0200

    Modify owner_id column

--- data.sql
+++ data.sql
@@ -34,6 +34,42 @@ VALUES ('Digimon');
 -- For all animals with NULL value in animals table, modify species_id value to be id of Pokemon in species table.
  UPDATE animals
  SET species_id = (
-  SELECT id from species
+  SELECT id FROM species
   WHERE name = 'Pokemon')
  WHERE species_id is NULL;
+
+-- Modify owner_id to match corresponding ids from owners table.
+UPDATE animals
+SET owner_id = (
+  SELECT id FROM owners
+  WHERE full_name = 'Sam Smith'
+)
+WHERE name = 'Agumon';
+
+UPDATE animals
+SET owner_id = (
+  SELECT id FROM owners
+  WHERE full_name = 'Jennifer Orwell'
+)
+WHERE name = 'Pikachu';
+
+UPDATE animals
+SET owner_id = (
+  SELECT id FROM owners
+  WHERE full_name = 'Bob'
+)
+WHERE name = 'Plantmon';
+
+UPDATE animals
+SET owner_id = (
+  SELECT id FROM owners
+  WHERE full_name = 'Melody Pond'
+)
+WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
+
+UPDATE animals
+SET owner_id = (
+  SELECT id FROM owners
+  WHERE full_name = 'Dean Winchester'
+)
+WHERE name = 'Angemon' OR name = 'Boarmon';
